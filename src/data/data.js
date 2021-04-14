48d4360c40d841e7d04fd38124d10b1c02d02c43
Mark Padilla <markjjpadilla@gmail.com>
2021-04-13 22:33:03 -0400

    Personalize general website data

--- src/data/data.js
+++ src/data/data.js
@@ -1,59 +1,22 @@
 module.exports = {
-  SiteTitle: 'Abdelali',
+  SiteTitle: 'Mark J.J. Padilla',
   Sitelogo: '#',
-  SiteLogoText: 'Abdelali',
-  SiteAuthor: 'Abdelali Dahir',
-  SiteDescription: 'Front End Developer',
-  defaultDescription: 'Software engineer!', 
-  githubApiQuery: `query ($number_of_repos: Int!) {
-    viewer {
-      name
-      avatarUrl
-      isHireable
-      resourcePath
-      repositories(last: $number_of_repos, privacy: PUBLIC, orderBy: { field: STARGAZERS, direction:ASC } ) {
-        nodes {
-          name
-          description
-          homepageUrl
-          forkCount
-          createdAt
-          updatedAt
-          resourcePath
-          languages(last: 1, orderBy: { field: SIZE, direction:ASC } ) {
-            edges {
-              node {
-                name
-                color
-              }
-            }
-          }
-          licenseInfo {
-            name
-          }
-          stargazers {
-            totalCount
-          }
-        }
-      }
-    }
-  }`,
-  githubApiVariables: {
-    number_of_repos: 12,
-  }, 
+  SiteLogoText: 'Mark J.J. Padilla',
+  SiteAuthor: 'Mark Jonel John Padilla',
+  SiteDescription: 'Web & Mobile Developer',
+  defaultDescription: 'Developer of user-focused software on web and mobile. Vivacious variety writer.', 
   SiteSocialLinks: {
-    twitter: 'https://twitter.com/AbdaliDahir',
-    github: 'https://github.com/AbdaliDahir',
-    linkedin: 'https://www.linkedin.com/in/abdelali-dahir-0b5344111/',
+    github: 'https://github.com/MarquisLP',
+    linkedin: 'https://www.linkedin.com/in/mark-jj-padilla/',
   },
   SiteAddress: {
-    city: 'Casablanca',
-    region: 'CurvaSud',
-    country: 'Morocco',
+    city: 'Toronto',
+    region: 'Ontario',
+    country: 'Canada',
     zipCode: 'ZipCode',
   },
   SiteContact: {
-    email: 'abdali.dahir@gmail.com',
+    email: 'mark@averistudios.com',
     phone: 'phone number',
   },
   SiteCopyright: '2020',
